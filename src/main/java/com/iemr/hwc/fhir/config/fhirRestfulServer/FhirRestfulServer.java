bb344d72cd4a76afb51a99b0dc579d9de956bb55
Shrey Patidar <shrey.p@beehyv.com>
2023-09-05 13:55:57 +0530

    Removed the extra FhirRestfulServer constructor.

--- src/main/java/com/iemr/hwc/fhir/config/fhirRestfulServer/FhirRestfulServer.java
+++ src/main/java/com/iemr/hwc/fhir/config/fhirRestfulServer/FhirRestfulServer.java
@@ -26,10 +26,6 @@ import java.util.List;
 public class FhirRestfulServer extends RestfulServer {
     private ApplicationContext applicationContext;
 
-    public FhirRestfulServer(){
-
-    }
-
     public FhirRestfulServer(ApplicationContext context) {
 
         this.applicationContext = context;
