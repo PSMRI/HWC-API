e96e479b1d08817b26680e281fd5893e9fd96aff
Ravi Shanigarapu <ravi.shanigarapu@wipro.com>
2025-04-17 15:22:43 +0530

    Swagger changes

--- src/main/java/com/iemr/hwc/utils/JwtUserIdValidationFilter.java
+++ src/main/java/com/iemr/hwc/utils/JwtUserIdValidationFilter.java
@@ -55,6 +55,9 @@ public class JwtUserIdValidationFilter implements Filter {
 		// Skip login and public endpoints
 		if (path.equals(contextPath + "/user/userAuthenticate")
 				|| path.equalsIgnoreCase(contextPath + "/user/logOutUserFromConcurrentSession")
+				|| path.startsWith(contextPath + "/swagger-ui")
+				|| path.startsWith(contextPath + "/v3/api-docs")
+				|| path.startsWith(contextPath + "/user/refreshToken")
 				|| path.startsWith(contextPath + "/public")) {
 			logger.info("Skipping filter for path: " + path);
 			filterChain.doFilter(servletRequest, servletResponse);
