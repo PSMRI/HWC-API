7800a92c3bb8923c0e9cbf6ff8961419686b6c8c
vishwab1 <vishwanath@navadhiti.com>
2025-08-06 13:42:29 +0530

    fix: updated userid authorization error

---
+++ src/main/java/com/iemr/hwc/utils/Constants.java
@@ -0,0 +1,9 @@
+package com.iemr.hwc.utils;
+
+public class Constants {
+	public static final String JWT_TOKEN = "Jwttoken";
+	public static final String USER_AGENT = "User-Agent";
+	public static final String OKHTTP = "okhttp";
+	
+	private Constants() {}
+}
