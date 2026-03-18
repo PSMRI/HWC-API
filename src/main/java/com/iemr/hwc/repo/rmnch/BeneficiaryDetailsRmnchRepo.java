811dce011b710da5263789c82e44c072a341f36b
Saurav Mishra <saurav.mishra@bizbrolly.com>
2026-03-18 12:10:03 +0530

    fix status of women

--- src/main/java/com/iemr/hwc/repo/rmnch/BeneficiaryDetailsRmnchRepo.java
+++ src/main/java/com/iemr/hwc/repo/rmnch/BeneficiaryDetailsRmnchRepo.java
@@ -2,10 +2,12 @@ package com.iemr.hwc.repo.rmnch;
 
 import com.iemr.hwc.data.rmnch.RMNCHBeneficiaryDetailsRmnch;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.math.BigInteger;
 @Repository
 public interface BeneficiaryDetailsRmnchRepo extends JpaRepository<RMNCHBeneficiaryDetailsRmnch,Long> {
-    RMNCHBeneficiaryDetailsRmnch findByBenRegId(BigInteger valueOf);
-}
+    @Query("SELECT r FROM RMNCHBeneficiaryDetailsRmnch r WHERE r.BenRegId = :benRegId")
+    RMNCHBeneficiaryDetailsRmnch findByBenRegId(@Param("benRegId") BigInteger benRegId);}
